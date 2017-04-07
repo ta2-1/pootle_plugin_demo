9444b25eb771a7560f309333ec1dd9aef2d487b5
Rimas Kudelis <rq@akl.lt>
2017-04-07 15:25:47 +0300

    Added a couple test PHP files

---
+++ test_files/javascript.en.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'turbo_app' => array(
+		'action_bar.collapse_all' => 'Collapse all',
+		'action_bar.deselect_all' => 'Deselect all',
+		'action_bar.expand_all' => 'Expand all',
+		'action_bar.select_all' => 'Select all',
+	),
+);
---
+++ test_files/javascript.lt.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'turbo_app' => array(
+		'action_bar.collapse_all' => 'Suskleisti visus',
+		'action_bar.deselect_all' => 'Atžymėti visus',
+		'action_bar.expand_all' => 'Išskleisti visus',
+		'action_bar.select_all' => 'Pažymėti visus',
+	),
+);
